8ddf9620df6c6e8cc5875f9efd13222a7a2000b6
hack0303 <1097037585@qq.com>
2021-03-04 13:54:09 +0800

    python insert-sort

--- src/python/insert_sort.py
+++ src/python/insert_sort.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
+# -*- coding: UTF-8 -*-
 
-print("Hello, World!")
+inArray=[4,3,5,123,12]
+
+def insert_sort(aArray):
+    print("origin array is ",aArray)
+    #range is [x,y) not is [x,y]
+    for x in range(1,len(aArray)):
+        key=aArray[x]
+        y=x-1;
+        while aArray[y] > key and y >=0:
+            aArray[y+1]=aArray[y]
+            y-=1
+        aArray[y+1]=key
+    print("after sort is ",aArray)
+
+
+
+insert_sort(inArray)

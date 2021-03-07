92fba3b2854232348b1b8bff69583255652a7416
hack0303 <1097037585@qq.com>
2021-03-07 14:37:37 +0800

    1.题目7

---
+++ src/main/java/org/cland/Case007.java
@@ -0,0 +1,30 @@
+package org.cland;
+
+public class Case007 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int x = 123;
+        System.out.println(solution.reverse(x));
+    }
+
+    /**
+     * */
+    static class Solution {
+        public int reverse(int x) {
+            if (x == 0) {
+                return 0;
+            }
+            long store = 0;
+            while (x > 0) {
+                store = store * 10 + x % 10;
+                x /= 10;
+            }
+            if (store > Math.pow(2, 31) - 1  || store < Math.pow(-2, 31)) {
+                return 0;
+            }
+            return (int) store;
+        }
+    }
+
+}
